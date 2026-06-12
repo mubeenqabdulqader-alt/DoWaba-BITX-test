67609f0240163aa6404a66ef7a1bf55f36a20146
Mubeen <mubeen@gmail.com>
2026-06-12 14:42:41 +0300

    changes res err

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-const BOT_TOKEN = "8980627295:AAE-bk9y2x6fh_RXFm2qjnc3CTRkecidEgk";
-const CHAT_ID = "886931442";
+const BOT_TOKEN = "YOUR_BOT_TOKEN";
+const CHAT_ID = "YOUR_CHAT_ID";
 
 app.all("/webhook/dowaba", async (req, res) => {
   try {
@@ -35,14 +35,23 @@ ${data.order || data.message || "N/A"}
       }
     );
 
-    res.send("OK");
+    return res.status(200).json({
+      success: true,
+      message: "Order received"
+    });
+
   } catch (err) {
-    console.log("ERROR:", err.message);
-    res.send("OK");
+    console.error(err);
+
+    return res.status(500).json({
+      success: false,
+      message: err.message
+    });
   }
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
